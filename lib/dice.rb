a41bb8235836407ec95e744964fcfe611c2713db
Reeshul <reeshul@hotmail.com>
2021-01-09 17:08:26 +0000

    Completed user stories

--- lib/dice.rb
+++ lib/dice.rb
@@ -1,7 +1,25 @@
 class Dice
 
-  def roll
-    return rand(6) + 1
+  def initialize
+    @history = []
+  end
+
+  def roll(n = 1)
+    rolled_results = []
+    n.times {
+      result = rand(6) + 1
+      @history << result
+      rolled_results << result
+    }
+    rolled_results
+  end
+
+  def view_history
+    @history
+  end
+
+  def current_score
+    @history.sum
   end
 
 end
--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -2,10 +2,31 @@ require 'dice'
 
 describe Dice do 
 
-  describe '#roll' do
+  context '#roll' do
 
     it 'returns a number between 1 and 6' do
-      expect(subject.roll).to be_between(1,6)
+      expect(subject.roll[0]).to be_between(1,6)
+    end
+
+    it 'returns a result that is randomly picked between 1 and 6' do
+      srand(9)
+      expect(subject.roll[0]).to eq 5
+    end
+
+    it 'returns a total between n*1 and n*6' do
+      srand(5)
+      expect(subject.roll(2).sum).to eq 10
+    end
+
+    it 'records each die roll' do
+      srand(3)
+      subject.roll(2)
+      expect(subject.view_history).to eq [3, 1]
+    end
+
+    it 'able to get current score' do
+      subject.roll
+      expect(subject.current_score).to eq 2
     end
 
   end
